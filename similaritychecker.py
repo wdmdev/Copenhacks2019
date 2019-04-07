03daa2823f5847cea11eff15174c8b6ef5e66537
Younes Subhi <yssubhi@gmail.com>
2019-04-07 07:59:45 +0200

    Update similaritychecker.py
    Check similarity between curriculum material and user input

--- similaritychecker.py
+++ similaritychecker.py
@@ -6,9 +6,52 @@ Created on Sun Apr  7 07:30:47 2019
 @author: Yssubhi
 """
 
+import speechsdk as ssdk
+import getWikiData as gWD
+import numpy as np
+import random
+import wikipedia as wiki
+import azure.cognitiveservices.speech as speechsdk
+import time
+import requests
+from pprint import pprint
+from IPython.display import HTML
+import settings
+import Question
 from difflib import SequenceMatcher
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-print(similar("Cult is an energy drink with a lot of caffiene and it helps you stay energized through out the day","Cult A / S is a Danish company with headquarters in Lystrup near Aarhus. The company produces energy drinks and cider products with and without alcohol. The company's best-known and sold drinks are CULT Shaker, Cult Raw Energy and Mokaï."))
+
+# Get WikiText
+CurriculumText = wiki.page(ssdk.top_doc).content
+
+# Compare texts
+
+subscription_key = settings.getSetting('text_key')
+assert subscription_key
+
+text_analytics_base_url = "https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/"
+
+key_phrase_api_url = text_analytics_base_url + "keyPhrases"
+print(key_phrase_api_url)
+
+documents = {'documents' : [
+  {'id': '1', 'text': KimText},
+  {'id': '2', 'text': CurriculumText}
+]}
+
+headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
+response  = requests.post(key_phrase_api_url, headers=headers, json=documents)
+key_phrases = response.json()
+pprint(key_phrases)
+
+table = []
+for document in key_phrases["documents"]:
+    text    = next(filter(lambda d: d["id"] == document["id"], documents["documents"]))["text"]    
+    phrases = ",".join(document["keyPhrases"])
+    table.append("<tr><td>{0}</td><td>{1}</td>".format(text, phrases))
+HTML("<table><tr><th>Text</th><th>Key phrases</th></tr>{0}</table>".format("\n".join(table)))
+
+print(similar(KimText,CurriculumText))
