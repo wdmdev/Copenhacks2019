821f9890639f4c21e9ee4346a36107f93054cdde
Younes Subhi <yssubhi@gmail.com>
2019-04-07 10:14:07 +0200

    Update RensponseGen.py

--- RensponseGen.py
+++ RensponseGen.py
@@ -8,20 +8,27 @@ Created on Sun Apr  7 09:47:30 2019
 
 import random
 
-question_format  = ['''Could you explain the {0} of {1} to me?''',
-                    '''That sounds interesting. I would like to know more about {0} of {1}Q''',
-                    '''{0}? What an exciting topic! Explain {0} of {1} to me?''',
-                    '''Never thought about {0} of {1}! Tell me more!''',
-                    '''Wuaw, {0} of {1}? So much to learn. Hit me!''']
+rand_q  = ['''Could you explain the {0} of {1} to me?''',
+           '''That sounds interesting. I would like to know more about {0} of {1}Q''',
+           '''{0}? What an exciting topic! Explain {0} of {1} to me?''',
+           '''Never thought about {0} of {1}! Tell me more!''',
+           '''Wuaw, {0} of {1}? So much to learn. Hit me!''']
 
-elaborate_format = ['''Hmm, could you elaborate on that?''',
-                    '''Could you try explaining it another way?''',
-                    '''I got a bit confused. Sorry! Wanna go again?''',
-                    '''Ooooooooh. Actually. I got nothing. Give me another chance''']
+rand_e = ['''Hmm, could you elaborate on that?''',
+          '''Could you try explaining it another way?''',
+          '''I got a bit confused. Sorry! Wanna go again?''',
+          '''Ooooooooh. Actually. I got nothing. Give me another chance''']
                     
 
-appraise_format = ['''Cool!''',
-                   '''Got it!''',
-                   '''Interesting!''',
-                   '''You always so smart''',
-                   '''ooooooooooh. Facinating''']
+rand_a = ['''Cool!''',
+          '''Got it!''',
+          '''Interesting!''',
+          '''You always so smart''',
+          '''ooooooooooh. Facinating''']
+
+question_format = random.choice(rand_q)
+
+elaborate_format = random.choice(rand_e)
+
+appraise_format = random.choice(rand_a)
+
